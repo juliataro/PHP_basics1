c29801105b23af392bd6b0e1d1d0cd66db072d1e
Julia <julia.taro@khk.ee>
2020-09-22 11:48:34 +0300

    Parandatud ul 1.5 lahendus

---
+++ 1/1.5Tundidearv.py
@@ -0,0 +1,8 @@
+#arvud
+ainepunkt = int(input("Sistege ainepunkt: "))
+ainepunkti_arv = int(input("Sisestage ainepunktide arv: "))
+õppiaine_pikkus = int(input("Sisestage õppimis pikkus: "))
+
+#arvutused
+ajakulu = (ainepunkt * ainepunkti_arv) / õppiaine_pikkus
+print(str(ajakulu))
