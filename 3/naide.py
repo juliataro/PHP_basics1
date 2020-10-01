825926103221bf7c8ef5b98215eb49783524b5e6
Julia <julia.taro@khk.ee>
2020-10-01 11:05:46 +0300

    2.2 Leedu perenimed

---
+++ 3/naide.py
@@ -0,0 +1,34 @@
+# üldtspkkel
+for kord in range(1, 6, 1):
+    print(str(kord) + ". tere")
+print("------")
+
+#teistpidi
+for kord in range(5, 0, -1):
+    print(str(kord) + ". tere")
+
+print("------")
+
+#eelkohntolliga tsukkel
+kord = 1
+while(kord <= 5):
+    print(str(kord + ". tere!"))
+    kord = kord + 1
+
+print("------")
+
+kord = 5
+while(kord < 0):
+    print(str(kord + ". tere!"))
+    kord = kord - 1
+
+print("------")
+
+kord = 1
+while(kord <= 5):
+    if (kord == 3):
+    kord += 1
+        print(str(kord + ". tere!"))
+       kord +=  1
+
+print(" ---break&continue_to_read")
