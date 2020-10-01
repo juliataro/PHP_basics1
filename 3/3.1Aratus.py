e886f094b112641f023978564ae1c78f5c15aba0
Julia <julia.taro@khk.ee>
2020-10-01 11:08:19 +0300

    2.2 Leedu perenimed

---
+++ 3/3.1Aratus.py
@@ -0,0 +1,12 @@
+kord = int(input("Sisestage mitu korda äratada: "))
+for k in range(1, kord+1, 1):
+    print("Tõuse ja sära!")
+
+    kord = int(input("Sisestage mitu korda äratada: "))
+    for k in range(0, 5):
+        print("Tõuse ja sära!")
+
+k = 1
+while(k <= kord):
+    print("Tüuse ja sära!")
+    k +=1
